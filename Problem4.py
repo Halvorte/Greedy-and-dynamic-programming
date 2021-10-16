e5cb97f4f21ed2910ae7f3604b180d0bac2f34da
Halvor Skribeland <halvorsk@uia.no>
2021-10-16 13:07:17 +0200

    Started on problem 4

--- Problem4.py
+++ Problem4.py
@@ -2,3 +2,28 @@
 Problem 4. Job scheduling
 Generate a random dataset with at least 50 jobs. Each job should have a profit and a deadline no later than 40. Find the maximum profit under the constraints of the deadlines for all jobs.
 '''
+import random
+
+# Function to create the 50 jobs with random profit and deadline
+def random_job_generation():
+    jobs = []
+    for i in range(50):
+        profit = random.randint(0, 20)
+        deadline = random.randint(1,40)
+        dictionary = {
+            'Job' : f'J{i+1}',
+            'Profit' : profit,
+            'Deadline' : deadline
+        }
+        jobs.append(dictionary)
+
+    return jobs
+
+
+if __name__ == '__main__':
+    print("yo")
+    # Getting the jobs
+    jobs = random_job_generation()
+
+    print(jobs)
+
