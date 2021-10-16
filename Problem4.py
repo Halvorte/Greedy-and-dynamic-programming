5c7c46530d222f079a19bb9450621d106beed2d9
Halvor Skribeland <halvorsk@uia.no>
2021-10-16 17:01:36 +0200

    Problem 4 is now working

--- Problem4.py
+++ Problem4.py
@@ -16,14 +16,47 @@ def random_job_generation():
             'Deadline' : deadline
         }
         jobs.append(dictionary)
-
+    # Return a list of all the jobs
     return jobs
+
+
+# Funciton to assign jobs to the latest possible slot.
+def assign_jobs(sorted_jobs):
+    schedule = [None] * 41
+    print(schedule)
+
+    for i in sorted_jobs:
+        deadline = i['Deadline']
+
+        for j in range(deadline):
+            if schedule[deadline - j] is None:
+                if (deadline - j) > 0:
+                    schedule[deadline - j] = i
+                    break
+
+
+    profit = 0
+    for k in schedule:
+        if k is None:
+            pass
+        else:
+            print(k)
+            profit += k['Profit']
+
+    print(profit)
+    print(schedule)
+
 
 
 if __name__ == '__main__':
     print("yo")
     # Getting the jobs
     jobs = random_job_generation()
-
     print(jobs)
 
+    # Sort jobs in order of profit
+    sorted_jobs = sorted(jobs, key=lambda x: x['Profit'], reverse=True)
+    print(sorted_jobs)
+
+    # Assign the jobs to a timeslot and print all the jobs
+    assign_jobs(sorted_jobs)
