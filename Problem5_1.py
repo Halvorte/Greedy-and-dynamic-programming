7d307822da101cbd00791b595b7c67b15c4a8184
Halvor Skribeland <halvorsk@uia.no>
2021-10-23 17:03:37 +0200

    fixed problem 5_1

--- Problem5_1.py
+++ Problem5_1.py
@@ -7,25 +7,31 @@ Use the weights, profits, and knapsack capacity from the P08 dataset to find the
 '''
 
 
-import random
-
 # Function to create items for fractional knapsack
-
 def items(nbr_of_items):
+    # Read weights from files
+    file1 = open('dataKnapsack/p08_w.txt', 'r')
+    weights = file1.readlines()
+    file1.close()
+    # Read profits from files
+    file2 = open('dataKnapsack/p08_p.txt', 'r')
+    profits = file2.readlines()
+    file2.close()
 
     items = []
 
     if nbr_of_items < 1:
         return
 
-    for i in range(1, nbr_of_items):
-        rand_value = random.randint(2, 20)
-        rand_weight = random.randint(1, 10)
+    for i in range(nbr_of_items):
+        value = int(profits[i])
+        weight = int(weights[i])
+
         dictionary = {
-            'Item' : i,
-            'Value' : rand_value,
-            'Weight' : rand_weight,
-            'V/W' : rand_value / rand_weight
+            'Item' : i + 1,
+            'Value' : value,
+            'Weight' : weight,
+            'V/W' : value / weight
         }
         items.append(dictionary)
 
@@ -42,6 +48,7 @@ def items_in_sack(items, sack_size):
     room_left = sack_size
     items_in_sack = []
     chosen_items = []
+    profit = 0
 
     for i in items:
         #print(f'Room left: {room_left}')
@@ -51,6 +58,7 @@ def items_in_sack(items, sack_size):
             room_left -= i['Weight']
             #print(f"yo: {i['Weight']}")
             chosen_items.append(f"{i}")
+            profit += i['Value']
         elif i['Weight'] > room_left and room_left > 0:
             # add (room_left / weight) * weight
             fraction_of_item = (room_left/i['Weight']) * i['Weight']
@@ -58,11 +66,13 @@ def items_in_sack(items, sack_size):
             room_left -= fraction_of_item
             #print(f"yo: {i['Weight']}")
             chosen_items.append(f" {fraction_of_item}  of {i}")
+            profit += (i['Value'] / i['Weight']) * fraction_of_item
         else:
             break
 
     # print(f'Items to pack: {items_in_sack}')
     print(f'Items: {chosen_items}')
+    print(f'Profit: {profit}')
 
     return chosen_items
 
@@ -72,14 +82,20 @@ def items_in_sack(items, sack_size):
 
 if __name__ == '__main__':
     # Size of sack
-    sack_size = 15
+    file3 = open('dataKnapsack/p08_p.txt', 'r')
+    capacity = file3.readline()
+    file3.close()
+    sack_size = int(capacity)
 
-    # Create random items with random value and weight. Also calculates value to weight ratio.
-    items_list = items(20)
+    # Create dictionaries for each item. Also calculates value to weight ratio.
+    items_list = items(24)
 
     # Sort items after highest value to weight ratio
     sorted_items = sorted_VW(items_list)
 
     # Choose the items to add to the sack using fractional knapsack problem.
     chosen_items = items_in_sack(sorted_items, sack_size)
+
+    # Print the chosen items and value
+
 
